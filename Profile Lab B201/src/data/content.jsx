0844b81148300ca730148c3f150cc3a78ee3b4ef
Fawaazdhawi <fawaazdhawi@gmail.com>
2025-09-28 12:54:30 +0700

    Foto dosen + tab image

--- Profile Lab B201/src/data/content.jsx
+++ Profile Lab B201/src/data/content.jsx
@@ -78,12 +78,12 @@ export const teamData = {
   ],
   teamTeachingTitle: 'Team Teaching',
   teamTeaching: [
-    { image: 'https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?w=300&h=300&fit=crop&crop=face', name: 'Dr. Ahmad Fuad Rahman, S.T., M.T.', role: 'Dosen Pengampu' },
-    { image: 'https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?w=300&h=300&fit=crop&crop=face', name: 'Dr. Surya Sumpeno, S.T., M.Sc.', role: 'Dosen Pengampu' },
-    { image: 'https://images.unsplash.com/photo-1519244703995-f4e0f30006d5?w=300&h=300&fit=crop&crop=face', name: 'Prof. Dr. Mauridhi Hery Purnomo, M.Eng.', role: 'Dosen Pengampu' },
-    { image: 'https://images.unsplash.com/photo-1560250097-0b93528c311a?w=300&h=300&fit=crop&crop=face', name: 'Prof. Dr. I Ketut Eddy Purnama, S.T., M.T.', role: 'Dosen Pengampu' },
-    { image: 'https://images.unsplash.com/photo-1494790108755-2616b5710000?w=300&h=300&fit=crop&crop=face', name: 'Dr. Susi Juniastuti, S.T.,M.Eng.', role: 'Dosen Pengampu' },
-    { image: 'https://images.unsplash.com/photo-1500648767791-00dcc994a43e?w=300&h=300&fit=crop&crop=face', name: 'Arta Kusuma Hernanda, S.T., M.T..', role: 'Dosen Pengampu' },
+    { image: 'https://www.its.ac.id/komputer/wp-content/uploads/sites/28/2020/07/198504032012121001_Departemen-Teknik-Komputer-682x1024.jpg', name: 'Dr. Ahmad Fuad Rahman, S.T., M.T.', role: 'Dosen Pengampu' },
+    { image: 'https://www.its.ac.id/komputer/wp-content/uploads/sites/28/2022/10/Pak-surya.png', name: 'Dr. Surya Sumpeno, S.T., M.Sc.', role: 'Dosen Pengampu' },
+    { image: 'https://www.its.ac.id/komputer/wp-content/uploads/sites/28/2020/07/196912091997031002_Departemen-Teknik-Komputer-1-682x1024.png', name: 'Mochamad Hariadi, S.T.,M.Sc.,Ph.D', role: 'Dosen Pengampu' },
+    { image: 'https://www.its.ac.id/komputer/wp-content/uploads/sites/28/2022/10/Pak-ketut.jpg', name: 'Prof. Dr. I Ketut Eddy Purnama, S.T., M.T.', role: 'Dosen Pengampu' },
+    { image: 'https://www.its.ac.id/komputer/wp-content/uploads/sites/28/2022/10/bu-susi.png', name: 'Dr. Susi Juniastuti, S.T.,M.Eng.', role: 'Dosen Pengampu' },
+    { image: 'https://www.its.ac.id/komputer/wp-content/uploads/sites/28/2023/11/Bakground-Putih.png', name: 'Arta Kusuma Hernanda, S.T., M.T..', role: 'Dosen Pengampu' },
   ],
   teamTeachingDescription: 'Tim pengajar yang berpengalaman dalam bidang telematika dan multimedia cerdas, yang berkomitmen untuk memberikan pendidikan berkualitas tinggi dan membimbing mahasiswa dalam pengembangan teknologi terdepan.',
 };
