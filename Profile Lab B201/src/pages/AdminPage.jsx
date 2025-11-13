45d161fc99940268e080b709f30cc1fb157144d6
Fawaazdhawi <fawaazdhawi@gmail.com>
2025-11-13 10:25:23 +0700

    tambah footer

--- Profile Lab B201/src/pages/AdminPage.jsx
+++ Profile Lab B201/src/pages/AdminPage.jsx
@@ -1,5 +1,6 @@
 import Admin from '../components/Admin.jsx';
 import Header from '../components/Header3.jsx';
+import Footer from '../components/Footer.jsx';
 
 const AdminPage = () => {
   return (
@@ -8,6 +9,7 @@ const AdminPage = () => {
         <main>
             <Admin />
         </main>
+        <Footer />
     </>
   );  
 };
